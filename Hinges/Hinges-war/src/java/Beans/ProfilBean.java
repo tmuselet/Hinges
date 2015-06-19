164d144388c622c1107bdffe43c8987e263c5d01
mhynnCXS <delmottemartin@gmail.com>
2015-06-19 08:35:22 +0200

    Création de ProfilBean

---
+++ Hinges/Hinges-war/src/java/Beans/ProfilBean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Martin
+ */
+@ManagedBean
+@ViewScoped
+public class ProfilBean {
+
+    /**
+     * Creates a new instance of ProfilBean
+     */
+    public ProfilBean() {
+    }
+    
+}
