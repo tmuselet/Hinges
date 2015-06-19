c1ed9a3a370592c6f04d1ed7044b398c87262097
mhynnCXS <delmottemartin@gmail.com>
2015-06-19 08:29:49 +0200

    Creation de ProfilManager et UserManager

---
+++ Hinges/Hinges-ejb/src/java/session/ProfilManager.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package session;
+
+import entities.Groupe;
+import entities.Profil;
+import java.util.List;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Martin
+ */
+@Stateful
+@LocalBean
+public class ProfilManager {
+    @PersistenceContext(unitName = "Hinges-ejbPU")
+    private EntityManager em;
+
+    public void persist(Object object) {
+        em.persist(object);
+    }
+    
+    public void add(Profil profil){
+        persist(profil);
+    }
+    
+    public Profil update(Profil profil){
+        return em.merge(profil);
+    }
+
+    public void delete(Profil profil){
+        em.remove(profil);
+    }
+    
+    public List<Profil> getAll(){
+        Query query = em.createNamedQuery("Profil.findAll");  
+        return query.getResultList();
+    }
+    
+    public List<Profil> getProfilsFromGroupe(Groupe groupe){
+        Query query = em.createNamedQuery("Profil.findAllByGroupe");  
+        query.setParameter("groupe", groupe);
+        return query.getResultList();
+    }
+    
+    
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
